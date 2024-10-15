ccead49ed704565e6b31a2b46c26943c1810d053
mayados <mariannedos.cours@gmail.com>
2024-10-15 22:02:19 +0200

    create page.tsx for route article with some elements for test

---
+++ app/article/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold uppercase my-5'>Mon titre</h1>
+        <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dicta temporibus aperiam odit mollitia sed ab, itaque cum voluptatum, molestias placeat corrupti reiciendis perspiciatis obcaecati accusamus esse dolor vero voluptatibus.
+        Tenetur blanditiis ipsam voluptate eius praesentium libero consequuntur! Optio soluta maxime eum aperiam suscipit et voluptatibus fugit earum laudantium veniam aliquid rerum non facilis, dicta obcaecati cupiditate dolores voluptatum perspiciatis.
+        Placeat explicabo repellendus ea reprehenderit dignissimos quam, vel in labore laudantium laboriosam fugiat nobis quis dolor a, corporis totam illum odio optio dolorem! Perspiciatis ab asperiores est perferendis temporibus nihil?
+        Eos facere adipisci at minima tenetur accusamus expedita distinctio deserunt. Numquam, iure. Culpa error laborum laboriosam nihil odit quae repellendus provident rem repudiandae iure, consequuntur pariatur perspiciatis voluptate iusto ratione?
+        Facilis sed sequi vitae consequatur ipsum, iure modi eius optio sapiente libero eveniet blanditiis, mollitia a, provident doloremque debitis! Enim architecto, laudantium consectetur nam placeat dignissimos atque facilis repellat dolores.</p>
+    </div>
+  )
+}
+
+export default page
