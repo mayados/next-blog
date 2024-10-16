4e3e4b755a3e9a2e903828e4a918a91f9d4d3036
mayados <mariannedos.cours@gmail.com>
2024-10-16 16:11:21 +0200

    use hooks (useState and useEffect), call to api

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,37 +1,59 @@
-import { db } from '@/lib/db'
-import React from 'react'
+// On transforme notre page en composant client et non plus serveur. Il faut faire ainsi car nous importons un composant qui a besoin de useEffect, il faut donc que l'un de ses parents soit marqué avec "use client"
+"use client"
+import { formatDate } from '@/lib/utils'
+import React, { useEffect, useState } from 'react'
 
-// En programmation JS côté serveur, nous travaillons en asynchrone
-const ArticlePage = async () => {
+const ArticlePage = () =>  {
+  //VERSION 1 : La liste des articles provient de la requête située dans app/api/article/route.ts, car nous l'appelons sous forme d'api
 
-  //Récupérer la liste des articles
-  // Ici on appel le nom du model (car on passe par l'ORM)
-  // En NoSQL, le findAll est findMany
-  // Ici on a apporté  le db que l'on a déclaré en global dans notre dossier lib (db.ts) grâce à ctrl + espace
-  //  L'accolade dans le findMany permet d'insérer des arguments (pour trier etc...)
-  const articles = await db.article.findMany({
-    orderBy: {
-      createdAt: 'desc'
+  // VERSION 2 : HOOKS
+  // Un setter avec un use state : est fait pour faire varier la valeur d'un élément
+  // Ici, le use effect est utilisé uniquement dans le cadre de la consommation d'api
+  // Toujours appeler le setter avec set+ le même nom que la constante elle-même
+  //  Ici par défaut, la collection est un tableau vide
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch('/api/article')
+      const data = await response.json()
+      // J'hydrate mon objet article avec les datas récupérés
+      setArticles(data)
     }
+
+    fetchArticles()
   })
 
+
+
   return (
-    <div>
+    // Mettre une div qui n'aura pas de style (par exemple de className) est une mauvaise pratique, c'est pour cela qu'on utilise les fragments <></> à la place. 
+    <>
         <h1>Blog</h1>
         {/* Liste des articles */}
         {
           // Liste des articles
           // Faire une boucle en react dans un composant
-          // Chaque parent de l'élément de la bouce doit être identifié par une clé (key), car ici chaque article doit être unique
+          // Chaque parent de l'élément de la boucle doit être identifié par une clé (key), car ici chaque article doit être unique
+          //  any est juste mis à des fins de test pour le moment. C'est une mauvaise pratique de typer les éléments aisni
           articles.map((article: any) => (
             <div className='mb-6' key="{article.id}">
               {/* Titre de l'article, nous l'appelons en fonction donné au champ en base de données */}
               <h2 className='text-emerald-700'>{article.title}</h2>
+              {/* Date et heure */}
+              <p>{formatDate(article.createdAt)}</p>
+              {/* Liste des tags d'un article */}
+              {
+                article.tags.map((tagArticle: any) => (
+                  <span key={tagArticle.tag.id}>
+                    {tagArticle.tag.name}
+                  </span>
+                ))}
               {/* Texte de l'article */}
               <p>{article.text}</p>
             </div>
           ))}
-    </div>
+    </>
   )
 }
 
