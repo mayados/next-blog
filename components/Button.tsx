c8713ed697325ce41b372de0bd99177c963d1ae4
mayados <mariannedos.cours@gmail.com>
2024-10-20 23:16:43 +0200

    add icon and actionButton props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+// On importe le type LucideIcon pour pouvoir faire passer l'icone voulue
+import {LucideIcon} from 'lucide-react'
+
 
 interface ButtonProps {
   label:string;
+  icon: LucideIcon;
+  actionButton?: () => void;
 }
 
-const Button:React.FC<ButtonProps> = ({label}) => {
+const Button:React.FC<ButtonProps> = ({label, icon: Icon, actionButton}) => {
   return (
-    <button className='px-5 py-2 mt-3 bg-cyan-900 hover:bg-cyan-800 cursor-pointer rounded-lg'>
+    <button className='flex f px-5 py-2 mt-3 bg-cyan-900 hover:bg-cyan-800 cursor-pointer rounded-lg' onClick={actionButton}>
         {/* On rend le texte du bouton variable */}
-        {label}
+        {label} 
+        <Icon className="mx-2" />
     </button>
   )
 }
