70cde1c48acd9b57bb11f2cb5c4a9803db0306ae
mayados <mariannedos.cours@gmail.com>
2024-10-20 23:13:16 +0200

    add code to delete comments

--- app/article/[articleId]/page.tsx
+++ app/article/[articleId]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Comment from '@/components/Comment'
 import Tag from '@/components/Tag'
 import React, { useEffect, useState } from 'react'
 
@@ -11,6 +11,8 @@ const ArticleDetailPage = ({params}: {params: {articleId: string}}) => {
     // | null => représente qu'on peut éventuellement récupérer quelque chose de null
     // Par défaut on définit le useState sur un type null (dernier mot de la ligne)
     const [article, setArticle] = useState<ArticleWithTagsAndComments | null>(null)
+    const [comments, setComments] = useState<CommentType[]>([]);
+
 
     useEffect(() => {
         const fetchArticle = async () => {
@@ -19,11 +21,29 @@ const ArticleDetailPage = ({params}: {params: {articleId: string}}) => {
             const data: ArticleWithTagsAndComments = await response.json()
             // On hydrate l'article avec les données récupérées
             setArticle(data)
+            // Car on a un use state pour les commentaire, on gérera leur état à part
+            setComments(data.comments); 
+
         }
         // On appelle la fonction
         fetchArticle()
         // Le useEffect va être redéclenché si l'articleId change
     }, [params.articleId])
+
+
+    const deleteComment = async (commentId: string) => {
+        try {
+            const response = await fetch(`/api/comment/${commentId}`, {
+                method: "DELETE",
+            });
+            if (response.ok) {
+                // Si l'appel à l'API fonctionne, on supprime le commentaire localement
+                setComments(prevComments => prevComments.filter(comment => comment.id !== commentId));
+            }
+        } catch (error) {
+            console.error("Erreur lors de la suppression du commentaire :", error);
+        }
+      }
 
   return (
     <div>
@@ -34,8 +54,17 @@ const ArticleDetailPage = ({params}: {params: {articleId: string}}) => {
             ))}
         </div>
         <p>{article?.text}</p>
+        <section>
+            <h2 className='my-3'>Commentaires ({comments.length})</h2>
+            {comments.map((comment) => (
+
+                <Comment key={comment.id} comment={comment} action={() => deleteComment(comment.id)}  />
+            ))}
+            
+        </section>
     </div>
   )
+
 }
 
 export default ArticleDetailPage
